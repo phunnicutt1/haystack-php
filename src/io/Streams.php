a4ef18a791d285ac9a59b9fddff116a543ad45dc
Patrick Hunnicutt <patrick@cxalloy.com>
2024-05-14 12:41:45 -0400

    Updated namespaces and composer auto loading

--- src/io/Streams.php
+++ src/io/Streams.php
@@ -1,105 +1,70 @@
 <?php
 namespace Cxalloy\Haystack\io;
 
-require_once 'Stream.php';
-/**
- * Translation Notes:
- * - Converted JavaScript code to PHP 8.3
- * - Preserved comments, method and variable names, and kept syntax as similar as possible
- * - Replaced JavaScript's `require` with PHP's `require_once`
- * - Replaced JavaScript's `module.exports` with PHP's `class` and `return` statements
- * - Replaced JavaScript's `inherits` with PHP's class inheritance
- * - Replaced JavaScript's `Stream` with PHP's `Stream` interface and `ReadableStream` and `WritableStream` classes
- * - Replaced JavaScript's `Buffer` with PHP's `fread` and `fwrite` functions
- * - Replaced JavaScript's `emit` with PHP's custom event handling using `call_user_func_array`
- */
 
+use GuzzleHttp\Psr7\Stream;
 
-
-class Reader extends ReadableStream
+class Reader extends Stream
 {
-    public function __construct($init = '')
-    {
-        parent::__construct();
-        $this->_data = $init;
-    }
+	public function __construct($init = '')
+	{
+		parent::__construct('php://temp', 'r+');
+		$this->write($init);
+	}
 
-    public function read($n = null)
-    {
-        $chunk = '';
-        $_n = ($n === null || $n === -1) ? null : $n;
-        $chunk = substr($this->_data, 0, $_n);
+	public function read($length = null) : string
+	{
+		if ($length === null) {
+			return $this->getContents();
+		}
 
-        $this->_data = substr($this->_data, $_n);
-        if ($_n >= strlen($this->_data) || $n === -1) {
-            $this->emit('end');
-        }
-        return $chunk;
-    }
+		$data = $this->read($length);
+		$this->seek($length, SEEK_CUR);
+		return $data;
+	}
 
-    public function pipe($dest)
-    {
-        $dest->end($this->read());
-        return $dest;
-    }
+	public function pipe($dest)
+	{
+		$dest->write($this->read());
+		return $dest;
+	}
 
-    public function write($data)
-    {
-        $this->_data .= $data;
-    }
+	public function end($data = null)
+	{
+		if ($data !== null) {
+			$this->write($data);
+		}
+		$this->emit('end');
+	}
 
-    public function end($data = null)
-    {
-        if ($data !== null) {
-            $this->write($data);
-        }
-        $this->emit('end');
-    }
-
-    public function __toString()
-    {
-        return $this->_data;
-    }
+	public function __toString() : string
+	{
+		return $this->getContents();
+	}
 }
 
-class Writer extends WritableStream
+class Writer extends Stream
 {
-    public function __construct($options = [])
-    {
-        parent::__construct($options);
-    }
+	public function __construct($options = [])
+	{
+		parent::__construct('php://temp', 'r+');
+	}
 
-    public function write($chunk, $encoding = null, $callback = null)
-    {
-        $ret = parent::write($chunk, $encoding, $callback);
-        if (!$ret) {
-            $this->emit('drain');
-        }
-        return $ret;
-    }
-
-    protected function _write($chunk, $encoding = null, $callback = null)
-    {
-        $this->write($chunk, $encoding, $callback);
-    }
-
-    public function __toString()
-    {
-        return $this->toBuffer();
-    }
-
-    public function toBuffer()
-    {
-        $buffers = [];
-        foreach ($this->_writableState->getBuffer() as $data) {
-            $buffers[] = $data['chunk'];
-        }
-
-        return implode('', $buffers);
-    }
+	public function write($data) : int
+	{
+		$ret = parent::write($data);
+		if (!$ret) {
+			$this->emit('drain');
+		}
+		return $ret;
+	}
 }
 
-return [
-    'Reader' => Reader::class,
-    'Writer' => Writer::class,
-];
+// Usage
+/*$reader = new Reader('Hello, World!');
+echo $reader->read(7); // Output: Hello,
+
+$writer = new Writer();
+$writer->write('Hello, ');
+$writer->write('World!');
+echo $writer; // Output: Hello, World!*/
